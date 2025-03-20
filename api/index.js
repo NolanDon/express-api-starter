061b171c51387d06a9eb668541d0da4a588c240c
Nolan <nolan.donb@gmail.com>
2025-03-20 20:39:49 +0800

    force redeploy

--- api/index.js
+++ api/index.js
@@ -52,7 +52,7 @@ router.get("/sign-upload", (req, res) => {
       timestamp,
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
-      folder // ✅ Send the correct folder back
+      folder // Cloudinary folder to target
     });
   } catch (error) {
     console.error("Error signing upload request:", error);
