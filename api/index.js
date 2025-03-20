b03d4d9ef1478d5623039fe009132a1ff80689d5
Nolan <nolan.donb@gmail.com>
2025-03-20 17:59:18 +0800

    fix missing middleware

--- api/index.js
+++ api/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
-
-// const emojis = require('./emojis');
-
+const middlewares = require('./middlewares');
 const router = express.Router();
-
 
 router.use(middlewares.notFound);
 router.use(middlewares.errorHandler);
@@ -20,3 +17,11 @@ router.get('/emojis', (req, res) => {
 
 
 module.exports = router;
+
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   /* eslint-disable no-console */
+//   console.log(`Listening: http://localhost:${port}`);
+//   /* eslint-enable no-console */
+// });
